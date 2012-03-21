058e23c2598e3aeb3d0192bca46ec06d522452e1
Fabio C. Canesin <fabio.canesin@gmail.com>
2012-03-20 22:06:01 -0300

    initial viscosity, its buggy.. going home pass 10PM

--- CPROP.py
+++ CPROP.py
@@ -150,7 +150,8 @@ class MartinHou(NRB):
         
     def rho(self, T, P):
         '''
-        Calculate the density (rho) frhom the temperature (T) and Pessure (P).
+        Calculate the density (rho [kg/m3]) frhom the temperature (T [K])
+        and Pessure (P [kPa]).
 
         The density is calculated iteratively using the Newton-Raphson method. 
         A ValueErrhor is raised if the method fails to converge.
@@ -255,6 +256,7 @@ class BaseFluid:
         '''
         SR = self.sK*((self.Pc*0.01)**self.sX)*(self.Tb**self.sY)*(self.Tc**self.sZ)
         Tr = T/self.Tc
+        if (Tr > 1): raise ValueError('Sig of supercritic fluid ?')
         Tbr = self.Tb/self.Tc
         return SR*(((1-Tr)/(1-Tbr))**self.sm)
 
@@ -267,24 +269,40 @@ class BaseFluid:
 
         >>> from CPROP import R134a
         >>> r = R134a()
-        >>> r.nu(T, P)
+        >>> r.eta(T, P)
         
         '''
-        Vcmol = self.Vc*1e6/(1000/self.MW)
-        
-        def mur(self):
-            """Compute the reduced dipole moment (mu [D])"""
-            return 131.3*(self.mu)/((self.Vcmol*self.Tc)**0.5)
-        
-        def etaSS(self):
-            """Compute eta**"""
-            return 
-        
-        def etaS(self):
-            """Compute eta*"""
-            return
 
-        return
+        Ts=1.2593*(T/self.Tc)
+        Vcmol = self.Vc*1e6/(1000/self.MW)
+        rhomol = self.rho(T, P)*(1000/self.MW)/1e6
+        y = rhomol*Vcmol/6
+        mur = 131.3*(self.mu)/((Vcmol*self.Tc)**0.5)
+        E1 = 6.324 +50.412*self.omega -51.680*(mur**4) +1189.0*self.kappa
+        E2 = 1.21e-3 -1.154e-3*self.omega -6.257e-3*(mur**4) +0.03728*self.kappa
+        E3 = 5.283 +254.209*self.omega -168.48*(mur**4) +3898.0*self.kappa
+        E4 = 6.623 +38.096*self.omega -8.464*(mur**4) +31.42*self.kappa
+        E5 = 19.745 +7.630*self.omega -14.354*(mur**4) +31.53*self.kappa
+        E6 = -1.9 -12.537*self.omega +4.985*(mur**4) -18.15*self.kappa
+        E7 = 24.275 +3.450*self.omega -11.291*(mur**4) +69.35*self.kappa
+        E8 = 0.7972 +1.117*self.omega +0.01235*(mur**4) -4.117*self.kappa
+        E9 = -0.2382 +0.06770*self.omega -0.8163*(mur**4) +4.025*self.kappa
+        E10 = 0.06863 +0.3479*self.omega +0.5926*(mur**4) -0.727*self.kappa
+        G1 = (1-(0.5*y))/((1-y)**3)        
+        G2 = (E1*((1-math.exp(-E4*y))/y)+E2*G1*math.exp(E5*y)+E3*G1)/(E1*E4+E2+E3)
+        omegav = 1.16145*(Ts**-0.14874) + 0.52487*math.exp(-0.77320*Ts)+ \
+                 2.16178*math.exp(-2.43787*Ts)
+        Fc = 1 -0.2756*self.omega +0.059035*(mur**4) + self.kappa
+        
+        def etaSS():
+            """Compute eta**"""
+            return  E7*(y**2)*G2*math.exp(E8+E9/Ts + E10/(Ts**2))
+        
+        def etaS():
+            """Compute eta*"""
+            return (Ts**0.5/omegav)*(Fc*(1/G2 + E6*y)) + etaSS()
+
+        return etaS()*36.344*((self.MW*self.Tc)**0.5)/(Vcmol**2/3.)
 
 #------------------------------------ Fluids ----------------------------------
 
@@ -307,10 +325,11 @@ class R134a(MartinHou, BaseFluid):
         self.Tb = 247.076 #[K] - Normal boiling point (at 1 atm)
         self.Tc = 374.23 #[K] - critical temperature
         self.Pc = 4060.3 #[kPa] - critical Pessure
+        self.Vc =  0.00194 #[m3/kg] - critical volume
         self.rhoc = 515.3 #[kg/m3] - critical density
-        self.volc =  0.00194 #[m3/kg] - critical volume
         self.R = 0.0815 #[kJ/kg*K] - gas constant
-        self.omega = 0.32684 #acentric factor
+        self.omega = 0.32684 #Acentric factor
+        self.kappa = 0.174823 #Association factor
         self.Tt = 169.85 #triple point temperature [K]
         self.hf = 200 #[kJ/kg] - reference enthalpy at 273.15K
         self.sf = 1 #[kJ/kg*K] - reference enTopy at 273.15K
